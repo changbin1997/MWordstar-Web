9213e2c782861b82edbf76a58f84193dfb923e7f
changbin1997 <zsmcbdyx@foxmail.com>
2020-09-23 23:44:50 +0800

    和 MWordStar 版本保持同步

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import 'bootstrap';  //  引入Bootstrap
-import './bootstrap.min.css';
-import './icon.css';  //  引入字体图标
-import './style.scss';  //  引入主题样式
-import app from './app';  //  引入主题JS
+//  引入Bootstrap
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/index';
+import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/dropdown';
+import './style/bootstrap.min.css';
+
+import './style/icon.css';  //  引入字体图标
+import './style/style.scss';  //  引入主题样式
+import app from './js/app';  //  引入主题JS
 import hljs from 'highlight.js/lib/highlight';  //  引入highlight.js
 
 //  引入代码高亮效果
@@ -19,6 +25,26 @@ import hlYaml from 'highlight.js/lib/languages/yaml';
 import hlCSS from 'highlight.js/lib/languages/css';
 import hlIni from 'highlight.js/lib/languages/ini';
 import hlSql from 'highlight.js/lib/languages/sql';
+import hlApache from 'highlight.js/lib/languages/apache';
+import hlCpp from 'highlight.js/lib/languages/cpp';
+import hlCoffeeScript from 'highlight.js/lib/languages/coffeescript';
+import hlDart from 'highlight.js/lib/languages/dart';
+import hlDockerfile from 'highlight.js/lib/languages/dockerfile';
+import hlGo from 'highlight.js/lib/languages/go';
+import hlJava from 'highlight.js/lib/languages/java';
+import hlKotlin from 'highlight.js/lib/languages/kotlin';
+import hlLess from 'highlight.js/lib/languages/less';
+import hlLua from 'highlight.js/lib/languages/lua';
+import hlMakefile from 'highlight.js/lib/languages/makefile';
+import hlMatlab from 'highlight.js/lib/languages/matlab';
+import hlNginx from 'highlight.js/lib/languages/nginx';
+import hlObjectiveC from 'highlight.js/lib/languages/objectivec';
+import hlPerl from 'highlight.js/lib/languages/perl';
+import hlPython from 'highlight.js/lib/languages/python';
+import hlRuby from 'highlight.js/lib/languages/ruby';
+import hlRust from 'highlight.js/lib/languages/rust';
+import hlSwift from 'highlight.js/lib/languages/swift';
+import hlTypeScript from 'highlight.js/lib/languages/typescript';
 
 import 'highlight.js/styles/vs2015.css';  //  引入highlight的样式
 
@@ -38,5 +64,25 @@ hljs.registerLanguage('yaml', hlYaml);
 hljs.registerLanguage('css', hlCSS);
 hljs.registerLanguage('ini', hlIni);
 hljs.registerLanguage('sql', hlSql);
+hljs.registerLanguage('apache', hlApache);
+hljs.registerLanguage('cpp', hlCpp);
+hljs.registerLanguage('coffeescript', hlCoffeeScript);
+hljs.registerLanguage('dart', hlDart);
+hljs.registerLanguage('dockerfile', hlDockerfile);
+hljs.registerLanguage('go', hlGo);
+hljs.registerLanguage('java', hlJava);
+hljs.registerLanguage('kotlin', hlKotlin);
+hljs.registerLanguage('less', hlLess);
+hljs.registerLanguage('lua', hlLua);
+hljs.registerLanguage('makefile', hlMakefile);
+hljs.registerLanguage('matlab', hlMatlab);
+hljs.registerLanguage('nginx', hlNginx);
+hljs.registerLanguage('objectivec', hlObjectiveC);
+hljs.registerLanguage('perl', hlPerl);
+hljs.registerLanguage('python', hlPython);
+hljs.registerLanguage('ruby', hlRuby);
+hljs.registerLanguage('rust', hlRust);
+hljs.registerLanguage('swift', hlSwift);
+hljs.registerLanguage('typescript', hlTypeScript);
 
 hljs.initHighlightingOnLoad();
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,80 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: './dist'
-                        }
-                    },
-                    {
-                        loader: 'css-loader'
-                    }
-                ]
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./dist",
             },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: './dist'
-                        }
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./dist",
             },
-            {
-                test: /\.eot$|\.svg$|\.ttf$|\.woff$/,
-                use: [
-                    {
-                        loader: 'url-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.css'
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.eot$|\.svg$|\.ttf$|\.woff$/,
+        use: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
     ],
-    devServer: {
-        contentBase: 'dist',
-        port: 8080,
-        historyApiFallback: true,
-        inline: true,
-        overlay: true
-    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new OptimizeCSSAssetsPlugin({}),
+  ],
+  devServer: {
+    contentBase: "dist",
+    port: 8080,
+    historyApiFallback: true,
+    inline: true,
+    overlay: true,
+  },
 };
